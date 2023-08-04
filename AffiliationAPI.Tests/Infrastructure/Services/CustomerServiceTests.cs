eaef75668da5e6d81b7eab59a5e6e44dc479684b
Lucas Gomez Aguero - CW <lucas.gomez@cfainstitute.org>
2023-08-04 11:18:33 -0300

    Service unit tests

--- AffiliationAPI.Tests/Infrastructure/Services/CustomerServiceTests.cs
+++ AffiliationAPI.Tests/Infrastructure/Services/CustomerServiceTests.cs
@@ -1,13 +1,63 @@
-﻿using System;
+﻿using AffiliationAPI.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Moq.Language;
+using Affiliation.Domain.Services;
+using Affiliation.Domain.Models;
+using FluentAssertions;
+using Affiliation.Domain.Dtos;
+using Affiliation.Infrastructure.Services;
+using Affiliation.Domain;
 
 namespace AffiliationAPI.Tests.Infrastructure.Services
 {
-    // TODO
     public class CustomerServiceTests
     {
+        [Fact]
+        public void CustomerService_Constructor_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new CustomerService(null));
+            Assert.Equal("unitOfWork", ex.ParamName);
+        }
+
+        [Fact]
+        public void Afiliation_Test()
+        {
+            // Arrange
+            var customerDto = Mock.Of<CustomerDto>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var affiliateRepo = new Mock<IRepository<Affiliate>>();
+            var affiliate = Mock.Of<Affiliate>();
+            affiliateRepo.Setup(r => r.Get(customerDto.AffiliateId)).Returns(affiliate);
+            unitOfWorkMock.Setup(uow => uow.Repository<Affiliate>()).Returns(affiliateRepo.Object);
+            
+            var customerRepo = new Mock<IRepository<Customer>>();
+            var customerModel = new Customer()
+            {
+                Name = customerDto.Name,
+                Id = 1
+            };
+            customerRepo.Setup(c => c.Insert(It.IsAny<Customer>())).Returns(customerModel);
+            unitOfWorkMock.Setup(uow => uow.Repository<Customer>()).Returns(customerRepo.Object);
+
+            var service = new CustomerService(unitOfWorkMock.Object);
+
+            // Act
+
+            var customer = service.Affiliation(customerDto);
+
+            // Assert
+            customer.
+                Should()
+                .NotBeNull()
+                .And
+                .Be(customer);
+        }
     }
 }
--- AffiliationAPI.Tests/Infrastructure/Services/ReportsServiceTests.cs
+++ AffiliationAPI.Tests/Infrastructure/Services/ReportsServiceTests.cs
@@ -1,13 +1,66 @@
-﻿using System;
+﻿using AffiliationAPI.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Moq.Language;
+using Affiliation.Domain.Services;
+using Affiliation.Domain.Models;
+using FluentAssertions;
+using Affiliation.Domain.Dtos;
+using Affiliation.Infrastructure.Services;
+using Affiliation.Domain;
 
 namespace AffiliationAPI.Tests.Infrastructure.Services
 {
     // TODO
     public class ReportsServiceTests
     {
+        [Fact]
+        public void ReportsService_Constructor_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new ReportsService(null));
+            Assert.Equal("unitOfWork", ex.ParamName);
+        }
+
+        [Fact]
+        public void Commission_Reporting_Single_Affiliate_and_Customer()
+        {
+            // Arrange
+            var customerDto = Mock.Of<CustomerDto>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            var affiliateRepo = new Mock<IRepository<Affiliate>>();
+            var affiliate = Mock.Of<Affiliate>();
+            var customer = Mock.Of<Customer>();
+            affiliate.Customers.Add(customer);
+            affiliateRepo.Setup(r => r.All()).Returns(new List<Affiliate> { affiliate }.AsQueryable());
+            unitOfWorkMock.Setup(uow => uow.Repository<Affiliate>()).Returns(affiliateRepo.Object);
+
+
+            var service = new ReportsService(unitOfWorkMock.Object);
+
+            var reportResultExpected = new List<ReferredCustomerDTO>() {
+                new ReferredCustomerDTO()
+                {
+                    Affiliate = affiliate.Name,
+                    ReferredCustomers = 1
+                }
+            };
+
+            // Act
+
+            var report = service.CommissionReporting();
+
+            // Assert
+            report.
+                Should()
+                .NotBeNull()
+                .And
+                .BeEquivalentTo(reportResultExpected);
+        }
     }
 }
